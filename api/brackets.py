096056227cdc3c3d3ad7e1001a09a50b7b461225
Facespamm <turrin.shura@gmail.com>
2026-02-03 00:24:25 +0500

    stash

--- api/brackets.py
+++ api/brackets.py
@@ -168,7 +168,7 @@ def fight_bracket(tournament_id):
             return jsonify({'success': False, 'message': 'Не выброна категория'}), 400
 
         tournament_repo = TournamentRepository()
-        tournament_category_id = tournament_repo.get_tournament_category(tournament_id, category_id)
+        tournament_category_id = tournament_repo.get_tournament_category_id(tournament_id, category_id)
 
         fight_repo = FightRepository()
         fights = fight_repo.get_fight_by_tournament(tournament_category_id)
@@ -183,8 +183,7 @@ def fight_bracket(tournament_id):
                 'white_athlete': athlete_repo.get_athlete_by_fight(fight.white_athlete_id, fight.id),
                 'tatami_number': fight.tatami_number,
                 'round': fight.round_number,
-                'status_fight': fight.status.value,
-                'next_fight': fight.next_fight,
+                'status_fight': fight.status.value
             })
 
         tournament_repo = TournamentRepository()
